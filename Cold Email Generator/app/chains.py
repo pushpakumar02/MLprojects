7be0bbdd98eb612ddfc0370b6f3022a08dab6439
Pushpa Kumar Balan <pushpakumar@Pushpas-MBP.lan>
2025-01-07 20:46:22 -0600

    Cold Email Generator

---
+++ Cold Email Generator/app/chains.py
@@ -0,0 +1,56 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+print(os.getcwd())
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+    
+            ### INSTRUCTION:
+            You are Pushpa Kumar Balan who is actively looking for internship opportunities in Software Development and Machine Learning Domain.
+            Your job is to write a cold email to the hiring team regarding the job mentioned above describing the capabilities in fulfilling their needs.
+            Also add the relevant skills and key words from the following: {link_list}
+            Remember you are Pushpa Kumar Balan who is actively looking for internship opportunities in Software Development and Machine Learning Domain.
+            Do not provide a PREAMBLE.
+            ### EMAIL (NO PREAMBLE):
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
